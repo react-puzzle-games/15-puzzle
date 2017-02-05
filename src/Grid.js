7b83d74d685f1e938289a01f8ee646319547f06e
ovidiubute <ovidiu.bute@gmail.com>
2017-02-05 07:11:00 -0500

    Remove grid border.

--- src/Grid.js
+++ src/Grid.js
@@ -5,7 +5,6 @@ import { tile as tileConstants } from './constants';
 const styles = {
   width: tileConstants.width * tileConstants.count,
   height: tileConstants.height * tileConstants.count,
-  border: '1px solid #FFAAAA',
   position: 'relative',
 };
 
