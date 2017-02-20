5715d9102c71e9df5fe495c08e80bcc76cc3f4aa
ovidiubute <ovidiu.bute@gmail.com>
2017-02-20 23:18:20 +0200

    Level generator mvp

---
+++ src/lib/levels-factory.js
@@ -0,0 +1,20 @@
+import { range, randomSubarray } from './utils';
+
+/**
+ * Generate a level of a given size
+ * @param {Number} size The total size of the level, must be a perfect square
+ * @returns {Object} TileSet object representing the level
+ */
+export default size => {
+  if (!Number.isInteger(Math.sqrt(size))) {
+    throw new Error(`Cannot generate level of size: <${size}>`);
+  }
+
+  if (size < 1 || size > 100) {
+    throw new Error(`Cannot generate level of size: <${size}>`);
+  }
+
+  return {
+    tileSet: randomSubarray(range(100), size),
+  };
+};
---
+++ src/lib/levels-factory.test.js
@@ -0,0 +1,22 @@
+import factory from './levels-factory';
+
+it('throws if given a size that is not a perfect square', () => {
+  expect(() => factory(13)).toThrowError();
+});
+
+it('throws if given a size below 1', () => {
+  expect(() => factory(0)).toThrowError();
+  expect(() => factory(-1)).toThrowError();
+});
+
+it('throws if given a size above 100', () => {
+  expect(() => factory(101)).toThrowError();
+});
+
+it('generates random levels of size 1', () => {
+  const level = factory(1);
+
+  expect(level.tileSet).toHaveLength(1);
+  expect(level.tileSet[0]).toBeGreaterThan(0);
+  expect(level.tileSet[0]).toBeLessThan(100);
+});
