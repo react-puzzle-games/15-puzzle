463cf51d9e37ea244d965af5abe304680e2206ed
ovidiubute <ovidiu.bute@gmail.com>
2017-03-04 22:14:38 +0200

    Add App snapshot test

---
+++ src/components/App.test.js
@@ -0,0 +1,16 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import App from './App';
+
+test('App renders correctly', () => {
+  const tree = renderer
+    .create(
+      <App
+        level={{
+          tileSet: [4, 6, 11, 1, 2, 16, 3, 15, 10, 9, 7, 13, 12, 14, 8, 5],
+        }}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
