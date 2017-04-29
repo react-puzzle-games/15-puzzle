484939d7e3978f713b18288aa6f2e96b0f6232ca
Dusan Sacha <sacha.dusan@gmail.com>
2017-04-29 09:51:52 +0200

    Feature/game statuses (#19)
    * Game over after solving
    
    * Update snapshot
    
    * changed GameOver function

---
+++ src/lib/game-status.js
@@ -0,0 +1,9 @@
+export const GAME_IDLE = Symbol('GAME_IDLE');
+export const GAME_STARTED = Symbol('GAME_STARTED');
+export const GAME_OVER = Symbol('GAME_OVER');
+
+export default {
+  GAME_IDLE,
+  GAME_STARTED,
+  GAME_OVER,
+};
