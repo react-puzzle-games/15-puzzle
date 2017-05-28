ae4044fe0a210c8f30d10585e0d669527d702403
ovidiubute <ovidiu.bute@gmail.com>
2017-05-28 22:52:00 +0300

    Fix CSS alignment

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -14,28 +14,27 @@ class ResetButton extends Component {
 }
 
 export default styled(ResetButton)`
-    text-align: center;
+  text-align: center;
 
-    & button {
-        border-radius: 8px;
-        padding: 6px 12px;
-        background: linear-gradient(to bottom, #ff6600 0%, #ff6600 100%);
-        font-weight: 300;
-        font-size: .8em;
-        border: none;
-        color: #fff;
-        cursor: pointer;
-    }
+  & button {
+    border-radius: 8px;
+    padding: 6px 12px;
+    background: linear-gradient(to bottom, #ff6600 0%, #ff6600 100%);
+    font-weight: 300;
+    font-size: .8em;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+  }
 
-    & button:active {
+  & button:active {
     box-shadow: inset 0px 1px 3px 2px rgba(0, 0, 0, 0.4);
-        cursor: pointer;
-    }
+    cursor: pointer;
+  }
 
-    & button:hover {
+  & button:hover {
     text-decoration: none;
     color: #fff;
     background: linear-gradient(to bottom, #226666 0%, #000 100%);
-    }
-
+  }
 `;
