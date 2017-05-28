19fac47881da7e60f366573c0be2453e7e6007f6
ovidiubute <ovidiu.bute@gmail.com>
2017-05-28 22:52:48 +0300

    Add a describe call

--- src/components/Moves.test.js
+++ src/components/Moves.test.js
@@ -2,14 +2,16 @@ import renderer from 'react-test-renderer';
 import React from 'react';
 import Moves from './Moves';
 
-describe('Moves renders correctly', () => {
-  test('before start', () => {
-    const tree = renderer.create(<Moves moves={0} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+describe('Moves', () => {
+  describe('should render correctly', () => {
+    test('before start', () => {
+      const tree = renderer.create(<Moves moves={0} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
 
-  test('mid-game', () => {
-    const tree = renderer.create(<Moves moves={42} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    test('mid-game', () => {
+      const tree = renderer.create(<Moves moves={42} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
